bbf523ace3a93ddfa90dd8c3e46ae1de3a3a559f
Chandni <chandni.sea@gmail.com>
2019-08-10 17:42:40 -0700

    Solution for chess

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace ChessBoard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Solution1");
+            Solution2();
+
+            Console.WriteLine();
+
+            Console.WriteLine("Solution2");
+
+            Solution1();
+
+        }
+
+        private static void Solution2()
+        {
+            for (int t = 0; t < 4; t++)
+            {
+
+                for (int i = 0; i < 4; i++)
+                {
+                    Console.Write("xo");
+
+                }
+                Console.WriteLine("");
+
+                for (int i = 0; i < 4; i++)
+                {
+                    Console.Write("ox");
+
+                }
+                Console.WriteLine("");
+
+            }
+        }
+
+        private static void Solution1()
+        {
+            for (int y = 0; y < 8; y++)
+            {
+
+
+
+                for (int r = 0; r < 8; r++)
+                {
+                    if ((r + y) % 2 == 0)
+                    {
+                        Console.Write("x");
+                    }
+                    else
+                    {
+                        Console.Write("o");
+                    }
+
+
+                }
+                Console.WriteLine();
+
+            }
+        }
+    }
+}
